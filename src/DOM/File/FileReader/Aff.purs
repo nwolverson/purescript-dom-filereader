b107fc07f41f1afca6f3b49650d4912c9fbcddcd
Tom Harding <tomjharding@live.co.uk>
2018-07-08 22:34:10 +0100

    Update for 0.12
    Bump dependencies, change module and function names, deal with changes
    in signatures.

--- src/DOM/File/FileReader/Aff.purs
+++ src/DOM/File/FileReader/Aff.purs
@@ -4,42 +4,42 @@ module DOM.File.FileReader.Aff
   , readAsDataURL
   ) where
 
-import Prelude
-import DOM.File.FileReader as FileReader
-import DOM.HTML.Event.EventTypes as EventTypes
-import Control.Monad.Aff (makeAff, Aff)
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Exception (error)
 import Control.Monad.Except (runExcept)
-import DOM (DOM)
-import DOM.Event.EventTarget (eventListener, addEventListener)
-import DOM.File.FileReader (result, fileReader)
-import DOM.File.Types (Blob, FileReader, fileReaderToEventTarget)
 import Data.ArrayBuffer.Types (ArrayBuffer)
 import Data.Either (Either(Left,Right), either)
-import Data.Foreign (F, Foreign, unsafeReadTagged, readString)
-import Data.Monoid (mempty)
+import Foreign (F, Foreign, unsafeReadTagged, readString)
+import Effect (Effect)
+import Effect.Aff (makeAff, Aff)
+import Effect.Exception (error)
+import Prelude
+import Web.Event.EventTarget (eventListener, addEventListener)
+import Web.File.FileReader (FileReader, result, fileReader, toEventTarget)
+import Web.File.FileReader as FileReader
+import Web.File.Blob (Blob)
+import Web.HTML.Event.EventTypes as EventTypes
 
-readAs :: forall eff a. (Foreign -> F a) -> (Blob -> FileReader -> Eff (dom :: DOM | eff) Unit) -> Blob -> Aff (dom :: DOM | eff) a
+readAs :: forall a. (Foreign -> F a) -> (Blob -> FileReader -> Effect Unit) -> Blob -> Aff a
 readAs readMethod getResult blob = makeAff \fun -> do
   let err = fun <<< Left
       succ = fun <<< Right
   fr <- fileReader
-  let et = fileReaderToEventTarget fr
-  addEventListener EventTypes.error (eventListener \_ -> err (error "error")) false et
-  addEventListener EventTypes.load (eventListener \_ -> do
-      res <- result fr
-      either (\errs -> err $ error $ show errs) succ $ runExcept $ readMethod res
-    ) false et
+  let et = toEventTarget fr
+
+  errorListener <- eventListener \_ -> err (error "error")
+  loadListener <- eventListener \_ -> do
+    res <- result fr
+    either (\errs -> err $ error $ show errs) succ $ runExcept $ readMethod res
+
+  addEventListener EventTypes.error errorListener false et
+  addEventListener EventTypes.load loadListener false et
   getResult blob fr
   pure mempty
 
-
-readAsText :: forall eff. Blob -> Aff (dom :: DOM | eff) String
+readAsText :: Blob -> Aff String
 readAsText = readAs readString FileReader.readAsText
 
-readAsArrayBuffer :: forall eff. Blob -> Aff (dom :: DOM | eff) ArrayBuffer
+readAsArrayBuffer :: Blob -> Aff ArrayBuffer
 readAsArrayBuffer = readAs (unsafeReadTagged "ArrayBuffer") FileReader.readAsArrayBuffer
 
-readAsDataURL :: forall eff. Blob -> Aff (dom :: DOM | eff) String
+readAsDataURL :: Blob -> Aff String
 readAsDataURL = readAs readString FileReader.readAsDataURL
